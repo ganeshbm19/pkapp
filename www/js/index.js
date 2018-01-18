2b7960f6ab87125dca54bf498e3bf927224bfe69
ganeshbm19 <35558391+ganeshbm19@users.noreply.github.com>
2018-01-18 19:24:05 +0530

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -47,3 +47,24 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("7fe9bf83-1ed6-426c-8ae2-672a1106b418")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
